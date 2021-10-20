e86d8194caf427f1f836d37539907048dd1997c9
noka <noka.hieunt@gmail.com>
2021-10-21 00:12:19 +0700

    update

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,5 @@
 //import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 import NextLink from 'next/link';
 import Image from 'next/image';
 import {
@@ -16,8 +16,13 @@ import Layout from '../../components/Layout';
 import useStyles from '../../utils/styles';
 import db from '../../utils/db';
 import Product from '../../models/Product';
+import axios from 'axios';
+import { Store } from '../../utils/Store';
+import { useRouter } from 'next/router';
 
 export default function ProductScreen(props) {
+  const router = useRouter();
+  const { dispatch } = useContext(Store);
   const { product } = props;
   const classes = useStyles();
   //const router = useRouter();
@@ -26,6 +31,15 @@ export default function ProductScreen(props) {
   if (!product) {
     return <div>Product Not Found!</div>;
   }
+  const addToCartHandler = async () => {
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock <= 0) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } });
+    router.push('/cart');
+  };
   return (
     <Layout title={product.name} description={product.description}>
       <div className={classes.section}>
@@ -97,7 +111,12 @@ export default function ProductScreen(props) {
                 </Grid>
               </ListItem>
               <ListItem>
-                <Button fullWidth variant="contained" color="primary">
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={addToCartHandler}
+                >
                   Add to card
                 </Button>
               </ListItem>
